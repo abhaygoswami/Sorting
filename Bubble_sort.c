d9cc4344b7c6b9d66a0dc53d521b527df3f79559
BoRiNG-CoDE <33592025+BoRiNG-CoDE@users.noreply.github.com>
2017-11-12 18:26:07 +0530

    Add files via upload

---
+++ Bubble_sort.c
@@ -0,0 +1,40 @@
+					//@<Boring_CODE>
+					//@Author: Abhay Goswami
+					
+					
+					// Bubble Sort C-Program
+#include <stdio.h>
+#define max 10
+
+int main()
+{
+	int temp;
+	int n;
+	printf("\nDon't Exceed than 10\n");
+	printf("\nEnter The Number: ");
+	scanf("\n%d",&n);
+	int arr[max];
+	//scanning the elements from user
+	for(int i=0;i<n;i++)
+		scanf("\n%d",&arr[i]);
+	
+	//Bubble sort
+	
+	for(int i=0;i<n;i++)
+	{
+		for(int j=0;j<n-i-1;j++)
+		{
+			if(arr[j]>arr[j+1])	
+			{
+				temp=arr[j];
+				arr[i]=arr[j+1];
+				arr[j+1]=temp;
+			}
+		}
+	}
+	
+	//print the sequence in increasing order
+	for(int i=0;i<n;i++)
+		printf("\n%d",arr[i]);
+	return 0;
+}
